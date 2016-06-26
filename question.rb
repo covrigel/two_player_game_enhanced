29e85790e9d62109b6785af8a0e41c4514c87224
covrigel <alexandra.nemes1986@gmail.com>
2016-06-26 21:16:05 +0000

    note to self: test before commit

--- question.rb
+++ question.rb
@@ -15,20 +15,18 @@ class Question
     
   def play(player)  
     
-    puts "It's your turn, #{player.name}"
-    puts "How much is #{@num1} #{@operator} #{@num2}?"
-    given_answer = gets.chomp
+  puts "It's your turn, #{player.name}"
+  puts "How much is #{@num1} #{@operator} #{@num2}?"
+  given_answer = gets.chomp
 
-    if given_answer.is_a? Integer
-      correct_answer = @num1.send(@operator, @num2) 
+    correct_answer = @num1.send(@operator, @num2) 
 
-      if correct_answer.to_i == given_answer.to_i
-        player.gain_points
-        puts "You are correct! You now have #{player.points} points!".green
-      else
-        player.lose_life
-        puts "That's not right! You lost a life, you are now at #{player.life}!".red
-      end
+    if correct_answer.to_i == given_answer.to_i
+      player.gain_points
+      puts "You are correct! You now have #{player.points} points!".green
+    else
+      player.lose_life
+      puts "That's not right! You lost a life, you are now at #{player.life}!".red
     end
   end
 
